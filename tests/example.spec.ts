14dd0c000fac090bf422b1c5e453cd09d690b0c8
NushkovaVeronika <veronikanushkova@protonmail.com>
2025-06-10 23:23:50 +0200

    Main page links test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test'; 
+import { test, expect } from '@playwright/test';
 // expect - used for assertion if something is true or not
 // await -  wait for the page to fully load before we continue 
 
@@ -8,17 +8,17 @@ import { test, expect } from '@playwright/test';
 // 3. check if the page contains the word 'Playwright'
 
 test('basic test', async ({ page }) => { // defines a test with a name 'basic test'
-  await page.goto('https://playwright.dev'); // this opens the url page
-  await expect(page).toHaveTitle(/Playwright/); // assert, check if the page has a word 'Playwright'
+    await page.goto('https://playwright.dev'); // this opens the url page
+    await expect(page).toHaveTitle(/Playwright/); // assert, check if the page has a word 'Playwright'
 });
 
 // 1. go to the page playwright.dev
 // 2. locate the element <header>
 // 3. check if its visible on the page ( if header does not exists, the test will fail)
 
-test.only('Header is visible on the homepage', async({page}) => {
-    await page.goto('https://playwright.dev'); 
-    
+test.only('Header is visible on the homepage', async ({ page }) => {
+    await page.goto('https://playwright.dev');
+
     const header = page.locator('header');
     await expect(header).toBeVisible();
 })
@@ -27,7 +27,7 @@ test.only('Header is visible on the homepage', async({page}) => {
 // 2. locate and click on the button 'Get Started'
 // 3. check if the URL is changed 
 
-test.only('Click on the Get started button', async({page}) => {
+test.only('Click on the Get started button', async ({ page }) => {
     await page.goto('https://playwright.dev');
     await page.click('text=Get started');
     await expect(page).toHaveURL(/\/docs\/intro/);
@@ -37,7 +37,7 @@ test.only('Click on the Get started button', async({page}) => {
 // 2. click on create an account button
 // 3. check if the URL changed
 
-test.only('Click on the Create an account button', async({page}) => {
+test.only('Click on the Create an account button', async ({ page }) => {
     await page.goto('https://magento.softwaretestingboard.com/');
     await page.click('text=Create an Account');
     await expect(page).toHaveURL(/\account\/create/);
@@ -49,7 +49,7 @@ test.only('Click on the Create an account button', async({page}) => {
 // 4. click on the button 'create an account' to register
 // 5. check if the account is succesfully created 
 
-test.only('Create an account', async({page}) => {
+test.only('Create an account', async ({ page }) => {
     await page.goto('https://magento.softwaretestingboard.com/');
     await page.click('text=Create an Account');
     await expect(page).toHaveURL(/\account\/create/);
@@ -73,16 +73,16 @@ test.only('Create an account', async({page}) => {
 // 5. check if the URL is changed
 // 6. check if there is a "Shorts" word somewhere on the page 
 
-test.only('Go to the women/bottoms/shorts section', async({page}) => {
+test.only('Go to the women/bottoms/shorts section', async ({ page }) => {
     await page.goto('https://magento.softwaretestingboard.com/');
 
     await page.hover('a#ui-id-4');
-    await page.waitForSelector('a:has-text("Bottoms")', {state:'visible'}); // without this the test fails because we need to wait before bottoms is shown from the dropdown menu
+    await page.waitForSelector('a:has-text("Bottoms")', { state: 'visible' }); // without this the test fails because we need to wait before bottoms is shown from the dropdown menu
     await page.hover('a:has-text("Bottoms")');
 
     await page.waitForSelector('a:has-text("Shorts")', { state: 'visible' });
     await page.click('a:has-text("Shorts")');
-    
+
     await expect(page).toHaveURL(/shorts/);
     await expect(page.locator('h1')).toHaveText('Shorts');
 });
@@ -93,7 +93,7 @@ test.only('Go to the women/bottoms/shorts section', async({page}) => {
 // 4. click on the add element button 2 times
 // 5. click on the delete button once
 
-test.only('Add/Remove elements', async({page}) => {
+test.only('Add/Remove elements', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/');
     await page.click('a:has-text("Add/Remove Elements")');
     await expect(page).toHaveURL(/add_remove_elements/);
@@ -108,7 +108,7 @@ test.only('Add/Remove elements', async({page}) => {
 // 2. enter username and password
 // 3. check if there is "Your username is invalid!" message somewhere on the screen
 
-test.only('Form authentication - invalid login', async({page}) => {
+test.only('Form authentication - invalid login', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/');
     await page.click('a:has-text("Form Authentication")');
     await page.fill('input[name="username"]', 'Veronika');
@@ -126,22 +126,33 @@ test.only('Form authentication - invalid login', async({page}) => {
 // 3. uncheck the second checkbox
 // 4. check if the first checkbox is selected or not
 
-test.only('Select/unselect checkboxes', async({page}) => {
+test.only('Select/unselect checkboxes', async ({ page }) => {
     await page.goto('https://the-internet.herokuapp.com/');
     await page.click('a:has-text("Checkboxes")');
 
     const checkbox1 = page.locator('input[type="checkbox"]').nth(0); // 1st checkbox with index 0
     const checkbox2 = page.locator('input[type="checkbox"]').nth(1) // 2nd checkbox with index 1
 
-    if(!(await checkbox1.isChecked())) {
+    if (!(await checkbox1.isChecked())) {
         await checkbox1.click();
     }
     await expect(checkbox1).toBeChecked();
 
-    if(!(await checkbox2.isChecked())) {
+    if (!(await checkbox2.isChecked())) {
         await checkbox2.click();
     }
     await expect(checkbox2).toBeChecked();
 
 });
 
+// check if links named "Form Authentication", "Checkboxes" & "Dropdown" exist on the main page
+// 1. go to the page https://the-internet.herokuapp.com/
+// 2. check if those links exist
+
+test.only('Check main page links', async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    await expect(page.locator('a', { hasText: 'Form Authentication' })).toBeVisible();
+    await expect(page.locator('a', { hasText: 'Checkboxes' })).toBeVisible();
+    await expect(page.locator('a', { hasText: 'Dropdown' })).toBeVisible();
+
+});
