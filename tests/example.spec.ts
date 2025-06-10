66f6db88ddb61fb2fe47336b480786c1ae6e3f7f
NushkovaVeronika <veronikanushkova@protonmail.com>
2025-06-10 02:23:53 +0200

    Add/Remove elements test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -86,3 +86,19 @@ test.only('Go to the women/bottoms/shorts section', async({page}) => {
     await expect(page).toHaveURL(/shorts/);
     await expect(page.locator('h1')).toHaveText('Shorts');
 });
+
+// 1. go to the page https://the-internet.herokuapp.com/
+// 2. click on the add/remove elements link
+// 3. check if the url is changed (contains add_remove_elements)
+// 4. click on the add element button 2 times
+// 5. click on the delete button once
+
+test.only('Add/Remove elements', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    await page.click('a:has-text("Add/Remove Elements")');
+    await expect(page).toHaveURL(/add_remove_elements/);
+    await page.click('button:has-text("Add Element")');
+    await page.click('button:has-text("Add Element")');
+    await page.click('button:has-text("Delete")');
+
+});
