d37f37548ed9846d20934fedfb325b40e7d7c2fd
NushkovaVeronika <veronikanushkova@protonmail.com>
2025-06-10 22:44:50 +0200

    Form authentication test - invalid login

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -102,3 +102,20 @@ test.only('Add/Remove elements', async({page}) => {
     await page.click('button:has-text("Delete")');
 
 });
+
+// form authentication invalid login check
+// 1. go to the page https://the-internet.herokuapp.com/
+// 2. enter username and password
+// 3. check if there is "Your username is invalid!" message somewhere on the screen
+
+test.only('Form authentication - invalid login', async({page}) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+    await page.click('a:has-text("Form Authentication")');
+    await page.fill('input[name="username"]', 'Veronika');
+    await page.fill('input[name="password"]', 'test123');
+    await page.click('button:has-text("Login")');
+
+    const errorMessage = page.locator('#flash') // #flash is the CSS selector when there is an error 
+    await expect(errorMessage).toContainText('Your username is invalid!');
+
+});
